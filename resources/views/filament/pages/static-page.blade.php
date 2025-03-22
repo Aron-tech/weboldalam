ba2a4f02b74049d663a2aa45fdb44ed366aa4326
Aron-tech <aron.papp2003@gmail.com>
2025-03-22 23:09:37 +0100

    Fix view

--- resources/views/filament/pages/static-page.blade.php
+++ resources/views/filament/pages/static-page.blade.php
@@ -1,6 +1,8 @@
 <x-filament::page>
     <form wire:submit.prevent="save">
         {{ $this->form }}
+        <div class="h-5 w-full">
+        </div>
         <x-filament::button type="submit">
             Mentés
         </x-filament::button>
